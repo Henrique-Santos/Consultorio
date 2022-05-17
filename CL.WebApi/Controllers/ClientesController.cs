60ecbca7ff609a0522009e29714083d12cc86993
Henrique Santos <henrique.144@outlook.com>
2022-05-17 15:58:19 -0300

    Initial commit

---
+++ CL.WebApi/Controllers/ClientesController.cs
@@ -0,0 +1,61 @@
+﻿using CL.Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CL.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        // GET: api/<ClientesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var clients = new List<Cliente>
+            {
+                new Cliente
+                {
+                    Id = 1,
+                    Nome = "João do Caminhão",
+                    DataNascimento = new DateTime(1970, 10, 01)
+                },
+                new Cliente 
+                {
+                    Id = 2,
+                    Nome = "Maria da Vizinha",
+                    DataNascimento = new DateTime(1970, 10, 01)
+                }
+            };
+            return Ok(clients);
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Consultorio
+ Aplicação corporativa com .NET Core
