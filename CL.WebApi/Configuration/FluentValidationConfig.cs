683716e37b872d59519d66030c13a899c238726d
Henrique Santos <henrique.144@outlook.com>
2022-05-18 16:42:15 -0300

    Startup
    Refatorado classe Startup, criado classes de configuração

---
+++ CL.WebApi/Configuration/FluentValidationConfig.cs
@@ -0,0 +1,22 @@
+﻿using CL.Manager.Validator;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+
+namespace CL.WebApi.Configuration
+{
+    public static class FluentValidationConfig
+    {
+        public static void AddFluentValidationConfiguration(this IServiceCollection services)
+        {
+            // Adicionando as classes de validação do Fluent Validator
+            services.AddControllers()
+                .AddFluentValidation(c =>
+                {
+                    c.RegisterValidatorsFromAssemblyContaining<NovoClienteValidator>();
+                    c.RegisterValidatorsFromAssemblyContaining<AlteraClienteValidator>();
+                    c.ValidatorOptions.LanguageManager.Culture = new CultureInfo("pt-br");
+                });
+        }
+    }
+}
--- CL.WebApi/Startup.cs
+++ CL.WebApi/Startup.cs
@@ -1,26 +1,11 @@
-using AutoMapper;
 using CL.Data.Context;
-using CL.Data.Repository;
-using CL.Manager.Implementation;
-using CL.Manager.Interfaces;
-using CL.Manager.Mappings;
-using CL.Manager.Validator;
-using FluentValidation.AspNetCore;
+using CL.WebApi.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CL.WebApi
 {
@@ -35,27 +20,17 @@ namespace CL.WebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // Adicionando as classes de validação do Fluent Validator
-            services.AddControllers()
-                .AddFluentValidation(c =>
-                { 
-                    c.RegisterValidatorsFromAssemblyContaining<NovoClienteValidator>();
-                    c.RegisterValidatorsFromAssemblyContaining<AlteraClienteValidator>();
-                    c.ValidatorOptions.LanguageManager.Culture = new CultureInfo("pt-br");
-                });
+            services.AddControllers();
 
-            services.AddAutoMapper(typeof(NovoClienteMappingProfile), typeof(AlteraClienteMappingProfile));
+            services.AddFluentValidationConfiguration();
+
+            services.AddAutoMapperConfiguration();
 
             services.AddDbContext<CLContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CLConnection")));
 
-            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddDependencyInjectionConfiguration();
 
-            services.AddScoped<IClienteManager, ClienteManager>();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Consultório", Version = "v1" });
-            });
+            services.AddSwaggerConfiguration();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -65,13 +40,7 @@ namespace CL.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
-            {
-                c.RoutePrefix = string.Empty;
-                c.SwaggerEndpoint("./swagger/v1/swagger.json", "CL V1");
-            });
+            app.UseSwaggerConfiguration();
 
             app.UseHttpsRedirection();
 
